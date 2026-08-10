ea5eb6e99689fa98527ddd102ced5cb0c72ed0a8
RayTMM <raytmmyt@gmail.com>
2026-08-09 23:06:55 -0400

    Initial commit

---
+++ src/main/java/net/raytmm/snaplook/SnaplookClient.java
@@ -0,0 +1,36 @@
+package net.raytmm.snaplook;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Minecraft;
+import net.raytmm.snaplook.keymappings.SnaplookKeyMapping;
+
+public class SnaplookClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient()
+    {
+        SnaplookKeyMapping.register();
+    }
+
+
+    public static void onEndTick(Minecraft client)
+    {
+        if (client.player == null)
+        {
+            return;
+        }
+
+        if (SnaplookKeyMapping.BACK_VIEW_KEYMAPPING.isDown())
+        {
+            client.options.setCameraType(CameraType.THIRD_PERSON_BACK);
+        }
+        else if (SnaplookKeyMapping.FRONT_VIEW_KEYMAPPING.isDown())
+        {
+            client.options.setCameraType(CameraType.THIRD_PERSON_FRONT);
+        }
+        else
+        {
+            client.options.setCameraType(CameraType.FIRST_PERSON);
+        }
+    }
+}
---
+++ src/main/java/net/raytmm/snaplook/keymappings/SnaplookKeyMapping.java
@@ -0,0 +1,21 @@
+package net.raytmm.snaplook.keymappings;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
+import net.raytmm.snaplook.Snaplook;
+import org.lwjgl.glfw.GLFW;
+
+public class SnaplookKeyMapping {
+
+    public static final KeyMapping BACK_VIEW_KEYMAPPING = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping("key.snaplook.snap_key_backwards", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, KeyMapping.Category.MISC));
+    public static final KeyMapping FRONT_VIEW_KEYMAPPING = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping("key.snaplook.nap_key_forwards", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_X, KeyMapping.Category.MISC));
+
+
+    public static void register()
+    {
+        Snaplook.LOGGER.info("Registering Mod Keybinds for " + Snaplook.MOD_ID);
+    }
+}
